8e7e6fcf5d9ed2d30b98ad1294c996e9175e5744
ali <83769855+qareebi@users.noreply.github.com>
2021-06-14 13:38:10 +0530

    Update timecalc.py

--- timecalc.py
+++ timecalc.py
@@ -1,39 +1,40 @@
 import os
-import wave
+import wave                                                 #add flac support ?
 import contextlib
 import csv
 
-audio_dir = [a for a in os.listdir() if a.endswith(".wav")] #
-audio_data = []
-time=[]
-#fname = '/tmp/test.wav'
-for audio in audio_dir: 
+audio_dir = [a for a in os.listdir() if a.endswith(".wav")] #directory to run time calc in, only reads .wavs, can be edited to read .flac later too
+audio_data = []                                             #stores the file names and their lenghts
+time=[]                                                     #stores the lenghts, too lazy to make a loop to read from audio_data
+total_time_s = 0                                            #time in seconds 
+
+for audio in audio_dir:                                     #loop to read all the time lenghts
     with contextlib.closing(wave.open(audio,'r')) as f:
         frames = f.getnframes()
         rate = f.getframerate()
         duration = frames / float(rate)
         audio_data.append([audio, duration])
         time.append(duration)
-total_time_s = 0        
+       
 for t in time:
     total_time_s+=t
 
+#h:m:s:ms
 total_time="{}:{}:{}:{}".format(int(total_time_s/3600),int(total_time_s/60),int(total_time_s%60),int(((total_time_s%1)*1000)))
 
 print(total_time)
 
 header = ['file name', 'duration']
 
-summary = ["total time", total_time]
+summary = ["total time", total_time]                       
+
+#might need to change csv output dir
 
 with open('duration_data.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
 
-    # write the header
     writer.writerow(header)
 
-    # write the data
     writer.writerows(audio_data)
     
-    # write the summary
     writer.writerow(summary)
